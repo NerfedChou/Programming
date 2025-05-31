017e288f389e6a011a33f3008f947ae0dd58415b
NerfedChou <janrhian012904@gmail.com>
2025-05-31 15:32:08 +0800

    String

---
+++ Basic/basicString.java
@@ -0,0 +1,23 @@
+package Basic;
+
+public class basicString {
+
+    public static void main(String[] args) {
+        // String is a sequence of characters
+        // It is immutable, meaning once created, it cannot be changed
+        // Strings can be created using double quotes
+        // Example of a string
+        String s = "Hello World";
+        // Print the string
+        System.out.println(s);
+
+        //You can also create a string directly on System.out.println
+        System.out.println("Hello World 2"); //
+
+        //Combining strings
+        String example = "Hello";
+        System.out.println(example + " World 3");
+
+    }
+
+}
